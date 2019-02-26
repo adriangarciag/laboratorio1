df3be45ce034f8b8ffa9976ba6a8fb05be31f9cc
GabyP.Agui <gabyagui150@gmail.com>
2019-02-26 09:33:19 -0600

    testcreated

---
+++ src/app/currencies/currencies.spec.ts
@@ -0,0 +1,9 @@
+import { currencies } from './currencies';
+describe( 'currencies', () => {
+    it('should return the supported currencies', () => {
+        const result = currencies();
+        expect(result).toContain('USD');
+        expect(result).toContain('AUD');
+        expect(result).toContain('EUR');
+    });
+});
